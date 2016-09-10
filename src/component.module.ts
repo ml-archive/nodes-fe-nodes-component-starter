dea6c97dde92124faf6425f47dc029f379ac2d15
Dennis Haulund Nielsen <dhni@nodes.dk>
2016-09-10 17:30:16 +0200

    Update 2016-09-10T15:30:10.873Z

--- src/component.module.ts
+++
@@ -1,9 +0,0 @@
-namespace component {
-	'use strict';
-	
-	const dependencies = [
-		'templates'
-	];
-
-	angular.module('component', dependencies);
-}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import './component.module';
-import './component.config';
-import './component.provider';
-import './component.filter';
-import './component.service';
-import './component.directive';
